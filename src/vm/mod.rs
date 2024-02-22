022d275a05b177ed3970103d0bb70df36bf2506f
manojkgorle <gorlemanojk@gmail.com>
2024-02-23 05:21:02 +0530

    done

--- src/vm/mod.rs
+++ src/vm/mod.rs
@@ -1,3 +1,18 @@
 pub mod instructions;
 pub mod registers;
 pub mod vm;
+
+use crate::MEMORY_SIZE;
+use instructions::execute;
+use vm::VM;
+
+pub fn execute_program(vm: &mut VM) {
+    while vm.registers.pc < MEMORY_SIZE as u16 {
+        let instr = vm.read_memory(vm.registers.pc.into());
+        if instr == 0 {
+            break;
+        }
+        vm.registers.pc += 1;
+        execute(instr, vm)
+    }
+}
